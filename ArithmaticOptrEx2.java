d53df0ef1a19ee8f308e7340c750adab845d8270
Santosh Mali <87570380+CodesSkm@users.noreply.github.com>
2022-01-10 11:07:42 +0530

    Add files via upload

---
+++ ArithmaticOptrEx2.java
@@ -0,0 +1,14 @@
+class ArithmaticOptrEx2 {
+	public static void main(String[] args) {
+		
+		int x = 5;
+		int y = 4;
+		int z = 3;
+		int k = 3;
+		if(x<y || z==k){
+			System.out.println("One Condition is true..!!");
+		}else{
+			System.out.println("Both condtion is flase..!!");
+		}
+	}
+}
